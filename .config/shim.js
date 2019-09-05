34adcbaa7214a4d568beb0f48b06829fa33bbdde
jnaruto <jnaruto168@gmail.com>
2019-09-05 09:51:52 +0800

    -a

---
+++ .config/shim.js
@@ -0,0 +1,15 @@
+/**
+ * MetaMask Support
+ *  |\_/|,,_____,~~`
+ *  (.".)~~     )`~}}
+ *	 \o/\ /---~\\ ~}}
+ *     _//    _// ~}
+ * 
+ * Copyright (c) 2019 jnaruto,eth-team
+ * E-mail : developer-team@jnaruto.io
+ * https://github.com/eth-lab/MicroPayPipe
+ *
+ */
+module.exports = {
+	"jQuery":{exports:"global:$"}
+}
