9d69a8940992410dc95cb2a408aed3937e955dd6
jnaruto <jnaruto168@gmail.com>
2019-09-06 14:17:32 +0800

    complete contracts deployed

--- migrations/2_deploy_library.js
+++ migrations/2_deploy_library.js
@@ -1,12 +1,32 @@
 var SafeMath = artifacts.require('SafeMath.sol');
 var Owned = artifacts.require('Owned.sol');
+var IERC20 = artifacts.require('IERC20.sol');
+var ERC20 = artifacts.require('ERC20.sol');
+var TCoinToken = artifacts.require('TCoinToken.sol');
+var MicroPaySystem = artifacts.require('MicroPaySystem.sol');
 
-module.exports = function(deployer){
-	deployer.deploy(SafeMath).then((d)=>{
-		//console.log(d);
-	});
-	deployer.deploy(Owned);
-	// deployer.deploy(IERC20);
-	// deployer.deploy(ERC20);
 
+module.exports = async function(deployer,network,accounts){
+	await deployer.deploy(SafeMath);
+	await deployer.deploy(Owned);
+
+	//deploy ERC20
+	await deployer.link(SafeMath,ERC20);
+	await deployer.deploy(ERC20);
+
+	//deloy Token
+	//deploy MicroPaySystem
+	await deployer.link(ERC20,TCoinToken);
+	await deployer.deploy(TCoinToken);
+
+	//deploy MicroPaySystem
+
+	await deployer.link(SafeMath,MicroPaySystem);
+	await deployer.link(Owned,MicroPaySystem);
+	let ta =await TCoinToken.new();
+	await deployer.deploy(MicroPaySystem,ta.address);	
+	//await deployer.deploy(MicroPaySystem,TCoinToken.address);	
+
+	console.log("Contracts Deployed on :"+network);
+	console.log("Acounts :"+accounts);
 }
